fe03d6c00bc07cc13c21eba8a0ab3bb2b8b2967e
atsalibram <atsalibaram@gmail.com>
2021-02-03 16:47:18 +0300

    changed quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,7 +13,7 @@ export class QuoteComponent implements OnInit {
     new Quote(2, 'The way to get started is to quit talking and begin doing.', '-	Walt Disney', 0, 0, new Date(2020,12,15), 'John'),
     new Quote(3, '.True happiness lies within yourself not on what others say about you.', 'Bramuel', 0, 0, new Date(2021,1,23), 'Legend'),
     new Quote(4, 'Life is what happens when you are busy making other plans.',  'John Lemon', 0, 0, new Date(2021,1,16), 'Chris'),
-    new Quote(5, 'Having hope for a better tomorrow is better than not hoping at all', '-	Bramuel', 0, 0, new Date(2021,1,28), 'Amariati')
+    new Quote(5, 'Having hope for a better tomorrow is better than not hoping at all.', '-	Bramuel', 0, 0, new Date(2021,1,28), 'Amariati')
   ]
 
   toogleDetails(index:any){
