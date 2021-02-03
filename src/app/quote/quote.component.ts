51af00e87dff46ec8cb2abcfc8fc74b9aa433cc5
atsalibram <atsalibaram@gmail.com>
2021-02-03 16:40:38 +0300

    changed author

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
   userQuotes = [
     new Quote(1, 'The greatest glory in living lies not in never falling, but in rising every time we fall.', '-	Nelson Mandela', 0, 0, new Date(2020,11,21), '8am'),
     new Quote(2, 'The way to get started is to quit talking and begin doing.', '-	Walt Disney', 0, 0, new Date(2020,12,15), 'John'),
-    new Quote(3, '.True happiness lies within yourself not on what others say about you', 'Bramuel', 0, 0, new Date(2021,1,23), 'Legend'),
+    new Quote(3, '.True happiness lies within yourself not on what others say about you.', 'Bramuel', 0, 0, new Date(2021,1,23), 'Legend'),
     new Quote(4, 'Life is what happens when you are busy making other plans',  'John Lemon', 0, 0, new Date(2021,1,16), 'Chris'),
     new Quote(5, 'Having hope for a better tomorrow is better than not hoping at all', '-	Bramuel', 0, 0, new Date(2021,1,28), 'Amariati')
   ]
